7979b06379288a89c60be1b9646796a9d75cd7cb
Yuli4n <j.contreras11@uniandes.edu.co>
2023-10-22 13:17:31 -0500

    Taller 2

--- main.ts
+++ main.ts
@@ -8,10 +8,17 @@ function mostradDatosSerie(serie:Serie):void{
     let tbodySerie=document.createElement("tbody");
 
     tbodySerie.innerHTML=`<tr><td>${serie.id}</td>
-    <td><a href=${serie.urlSerie}>${serie.titulo}</a></td>
+    <td><a href=# id="${serie.titulo}">${serie.titulo}</a></td>
     <td>${serie.canal}</td>
-    <td>${serie.temporadas}</td>`
+    <td>${serie.temporadas}</td>`;
     serieTable.appendChild(tbodySerie);
+
+    const serieLink= document.getElementById(serie.titulo) as HTMLAnchorElement;
+
+    serieLink.addEventListener('click', (event)=>{
+        event.preventDefault();
+        mostrarTarjeta(serie);
+    })
 }
 
 function mostrarDatosTodasSeries(series: Serie[]):void{
@@ -31,4 +38,18 @@ function mostrarDatosTodasSeries(series: Serie[]):void{
     //Aproveché este recorrido y esta función para añadir el promedio de temporadas
 }
 
+function mostrarTarjeta(serie:Serie):void{
+    const fichaSerie = document.getElementById('fichaSerie')!;
+
+    fichaSerie.innerHTML= `<div class="card">
+                                <img src="${serie.urlImagen}" alt="imagen representativa de la serie">
+                                <div class="card-body">
+                                    <h5 class="card-title">${serie.titulo}</h5>
+                                    <p class="card-text">${serie.descripcion}</p>
+                                    <a href="${serie.urlSerie}">Click aquí para visitar la serie</a>
+                                </div>
+                            </div>`
+    fichaSerie.style.display = 'block';
+}
 mostrarDatosTodasSeries(series);
+
--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,13 @@ import { series } from "./data.js";
 var serieTable = document.getElementById("series");
 function mostradDatosSerie(serie) {
     var tbodySerie = document.createElement("tbody");
-    tbodySerie.innerHTML = "<tr><td>".concat(serie.id, "</td>\n    <td><a href=").concat(serie.urlSerie, ">").concat(serie.titulo, "</a></td>\n    <td>").concat(serie.canal, "</td>\n    <td>").concat(serie.temporadas, "</td>");
+    tbodySerie.innerHTML = "<tr><td>".concat(serie.id, "</td>\n    <td><a href=# id=\"").concat(serie.titulo, "\">").concat(serie.titulo, "</a></td>\n    <td>").concat(serie.canal, "</td>\n    <td>").concat(serie.temporadas, "</td>");
     serieTable.appendChild(tbodySerie);
+    var serieLink = document.getElementById(serie.titulo);
+    serieLink.addEventListener('click', function (event) {
+        event.preventDefault();
+        mostrarTarjeta(serie);
+    });
 }
 function mostrarDatosTodasSeries(series) {
     var count = 0;
@@ -17,5 +22,11 @@ function mostrarDatosTodasSeries(series) {
     var averageSerie = document.createElement("tr");
     averageSerie.innerHTML = "Seasons average: ".concat(average);
     serieTable.appendChild(averageSerie);
+    //Aproveché este recorrido y esta función para añadir el promedio de temporadas
+}
+function mostrarTarjeta(serie) {
+    var fichaSerie = document.getElementById('fichaSerie');
+    fichaSerie.innerHTML = "<div class=\"card\">\n                                <img src=\"".concat(serie.urlImagen, "\" alt=\"imagen representativa de la serie\">\n                                <div class=\"card-body\">\n                                    <h5 class=\"card-title\">").concat(serie.titulo, "</h5>\n                                    <p class=\"card-text\">").concat(serie.descripcion, "</p>\n                                    <a href=\"").concat(serie.urlSerie, "\">Click aqu\u00ED para visitar la serie</a>\n                                </div>\n                            </div>");
+    fichaSerie.style.display = 'block';
 }
 mostrarDatosTodasSeries(series);
